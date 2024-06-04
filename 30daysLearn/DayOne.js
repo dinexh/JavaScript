7ec979a099cf489bfd6e8871aab34796a6ac72b3
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-06-04 12:03:06 +0530

    Javascript DayOne Operations and basics Delartion | printing using onload and getID

---
+++ 30daysLearn/DayOne.js
@@ -0,0 +1,14 @@
+function Declaration()
+        {
+            let a = 10.05;
+            let Name = "jane";
+            let Age = 18;
+            console.log("Name:" + name + "Age:" + Age + "Number:" + a);
+        }
+        function Operarions()
+        {
+            let a = 10;
+            let b = 40;
+            let sum = a+b;
+            let 
+        }
