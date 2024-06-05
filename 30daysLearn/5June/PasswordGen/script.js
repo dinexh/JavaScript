e02a38a10ff42f5d4e8414aa0e5ad5760c1ef571
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-06-05 17:14:35 +0530

    Fixed bug in show and hide

--- 30daysLearn/5June/PasswordGen/script.js
+++ 30daysLearn/5June/PasswordGen/script.js
@@ -1,21 +1,22 @@
-function generatepasword()
-{
-    const lenght = 12;
+function generatepassword() {
+    const length = 12;
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-    let password = " ";
-    for(let i = 0;i<lenght;i++)
-    {
+    let password = "";
+    for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
         password += charset.charAt(randomIndex);       
     }
     document.getElementById("Password").value = password;
 }
-function togglepasword(){
-    const passwordinput = document.getElementById("Password");
-    const show = document.getElementById("visiblebutton");
-    if(passwordinput.type === "password")
-        {
-            passwordInput.type = "text";
-            visiblebutton.textContent = "Hide";           
-        }
+
+function togglepassword() {
+    const passwordInput = document.getElementById("Password");
+    const showButton = document.getElementById("visiblebutton");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showButton.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        showButton.textContent = "Show";
+    }
 }
